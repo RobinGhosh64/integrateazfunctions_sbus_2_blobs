d26594b10fd023147f51218f2f9b6b50e308aa78
Robin Ghosh <roghos@microsoft.com>
2021-01-11 16:53:57 -0500

    Second Draft

---
+++ FunctionApp1/OurUtility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.IO;
+
+namespace FunctionApp1
+{
+    public class OurUtility
+    {
+
+        public static System.IO.Stream GenerateStreamFromString(string s)
+        {
+            System.IO.MemoryStream stream = new System.IO.MemoryStream();
+            StreamWriter writer = new System.IO.StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
