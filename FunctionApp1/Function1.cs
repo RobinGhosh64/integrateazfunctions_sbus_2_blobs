8788879ca09bed00f6402d002d776c0e48701ada
Robin Ghosh <roghos@microsoft.com>
2021-01-11 16:52:47 -0500

    Second Draft

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -12,19 +12,18 @@ namespace FunctionApp1
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static async System.Threading.Tasks.Task RunAsync([ServiceBusTrigger("orders", Connection = "ServiceBusConnection",IsSessionsEnabled = true)]Microsoft.Azure.ServiceBus.Message[] orders, ILogger log)
+        public static async System.Threading.Tasks.Task RunAsync([ServiceBusTrigger("orders", Connection = "ServiceBusConnection",IsSessionsEnabled = true)]Microsoft.Azure.ServiceBus.Message[] orders, ILogger log, Microsoft.Azure.ServiceBus.Core.MessageReceiver messageReceiver)
         {
 
-
+            var scope = new System.Transactions.TransactionScope(System.Transactions.TransactionScopeOption.RequiresNew, System.Transactions.TransactionScopeAsyncFlowOption.Enabled);
+        
+                
             foreach (Microsoft.Azure.ServiceBus.Message message in orders)
             {
                 log.LogInformation($"C# ServiceBus queue trigger function processed message: {Encoding.Default.GetString(message.Body)}");
 
                 string data = Encoding.Default.GetString(message.Body);
-
-
-                //
-                string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+                //string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
 
                 // Create a BlobServiceClient object which will be used to create a container client
                 BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
@@ -35,6 +34,8 @@ namespace FunctionApp1
                 // Create the container and return a container client object
                 BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
 
+
+
                 string localFilePath =  "upstest" + Guid.NewGuid().ToString();
 
                 // Get a reference to a blob
@@ -43,22 +44,14 @@ namespace FunctionApp1
                 Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
 
                 // Open the file and upload its data
-             
-                await blobClient.UploadAsync(GenerateStreamFromString(data));
-                
 
+                await blobClient.UploadAsync(FunctionApp1.OurUtility.GenerateStreamFromString(data));
 
+                // Done with this message
+                await messageReceiver.CompleteAsync(message.SystemProperties.LockToken);
             }
+            scope.Complete();
         }
 
-        private static System.IO.Stream GenerateStreamFromString(string s)
-        {
-            System.IO.MemoryStream stream = new System.IO.MemoryStream();
-            StreamWriter writer = new System.IO.StreamWriter(stream);
-            writer.Write(s);
-            writer.Flush();
-            stream.Position = 0;
-            return stream;
-        }
     }
 }
